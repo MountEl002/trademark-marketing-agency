0a630e5461d4103fc3f1079a7e87c974f5e6660d
MountEl002 <Ojuokelias@gmail.com>
2025-01-25 15:35:01 +0300

    Created pages for admin/customer and admin/orders

---
+++ src/app/admin/customers/[customerNumber]/finance/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Finannce = () => {
+  return <div>Finannce</div>;
+};
+
+export default Finannce;
---
+++ src/app/admin/orders/customer-drafts/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustonmerDrafts = () => {
+  return <div>CustomerDarafts</div>;
+};
+
+export default CustonmerDrafts;
