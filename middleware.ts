7857ea7e7e4d878de0f08af7c002b70668bdc465
Elias Ojouk <ojuokelias@gmail.com>
2025-12-24 19:09:33 +0300

    Remove session check and redirect to /login in middleware

--- middleware.ts
+++ middleware.ts
@@ -4,11 +4,6 @@ import type { NextRequest } from "next/server";
 export async function middleware(request: NextRequest) {
   const session = request.cookies.get("session");
 
-  // Return to /login if there's no session
-  if (!session) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
   return NextResponse.next();
 }
 
